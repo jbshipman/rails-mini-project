131b1d252ccb72e5eaf732afe136240415523705
jbshipman <jbshipman@icloud.com>
2020-02-25 16:12:49 -0600

    models and controllers boilerplated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
+  get 'tasks/create'
+  get 'tasks/update'
+  get 'tasks/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
